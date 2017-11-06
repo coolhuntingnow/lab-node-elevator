a73021da0fdf931edfdff0cdc764cffe5d0645cc
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-11-06 18:16:26 +0100

    Iteracion 1

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,20 +1,59 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
+    this.floor      = 0; //POSICIÓN ACTUAL DEL ASCENSOR
+    this.direction = "up";
     this.MAXFLOOR   = 10;
     this.requests   = [];
   }
-  
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    let listen = setInterval(() => {
+      this.update();
+    },1000);
+  }
+
+//The start function should start a setInterval call the update function every second
+
+  stop() {
+    clearInterval(this.listen);
+    } //The stop function should stop the elevator's setInterval listening for requests
+
+  update() {
+    this.log ();
+  }
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
+
+  floorUp() {
+   this.direction = up;
+   return this.direction;
+  }
+
   floorDown() { }
   call() { }
-  log() { }
+
+  log() {
+   console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
+
+
+
+// The Elevator class
+//
+// First, the elevator needs methods to move.
+//
+// In order to make it move, it will need a direction and a current position (floor). Let's focus on four main methods:
+//
+// *The update function should (for now) display the current status of the elevator by calling the log function.
+
+// The log function should just print the information related with position and direction:
+// Direction: up | Floor: 0
+// The start function should start a setInterval call the update function every second
+// The stop function should stop the elevator's setInterval listening for requests
+// These last two functions are key to the exercise.
+//
+// Remember to use JavaScript functions such as setTimeout() to create the listener
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator = new Elevator();
+
+
+elevator.start();
+elevator.stop();
